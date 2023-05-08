54bc0111c8d58caa008ae462cd7c3663b3994b64
sunny.singh <sunny.singh@cgi.com>
2023-05-07 21:34:15 -0400

    Practicing functional components React

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 import React from "react"
+// The above allows JSX to kick in and fdo the magic
 import ReactDOM from "react-dom"
-function MyApp(){
+// Playiing with functional componenets
+function MyInfo(){
+    let divStyle = {
+        color: 'blue',      
+      };
+      let h1Style = {color:'Tomato'};
+    // let h1Style = {"color:Tomato; text-align:center"}
+        
+
     return(
-        <ul>
+        <div>
+        <h1 style={h1Style}>Sunny Singh</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adip.
+        Lorem ipsum dolor sit amet, consectetur adip
+        Lorem ipsum dolor sit amet, consectetur adip</p>
+        
+        <p>Vacation places:</p>
+        <ul style={divStyle}>
             <li>Italy</li>
-            <li>France</li>
+            <li>Sweden</li>
+            <li>Finland</li>
         </ul>
+        </div>
     )
 }
-ReactDOM.render(<MyApp/>,document.getElementById("root"))
+ReactDOM.render(<MyInfo/>,document.getElementById("root"))
