a0694a7cc65c2cd84e3f133002c600b214c33e96
sunny.singh <sunny.singh@cgi.com>
2023-09-16 20:37:26 -0400

    Added new concepts learnt-Parent/Child,moving component to diff directory

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Footer() {
+    return(
+        <footer><h3>Created by 98sunny</h3></footer>
+
+    )
+}
+export default Footer
--- src/index.js
+++ src/index.js
@@ -2,28 +2,8 @@ import React from "react"
 // The above allows JSX to kick in and fdo the magic
 import ReactDOM from "react-dom"
 // Playiing with functional componenets
-function MyInfo(){
-    let divStyle = {
-        color: 'blue',      
-      };
-      let h1Style = {color:'Tomato'};
-    // let h1Style = {"color:Tomato; text-align:center"}
-        
+import MyInfo from "./components/MyInfo"
+import App from "./App"
 
-    return(
-        <div>
-        <h1 style={h1Style}>Sunny Singh</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adip.
-        Lorem ipsum dolor sit amet, consectetur adip
-        Lorem ipsum dolor sit amet, consectetur adip</p>
-        
-        <p>Vacation places:</p>
-        <ul style={divStyle}>
-            <li>Italy</li>
-            <li>Sweden</li>
-            <li>Finland</li>
-        </ul>
-        </div>
-    )
-}
-ReactDOM.render(<MyInfo/>,document.getElementById("root"))
+
+ReactDOM.render(<App/>,document.getElementById("root"))
